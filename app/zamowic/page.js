60811265e14254286498910b7dd61a98622e32df
Михаил Лядский <lmp200840@gmail.com>
2026-02-13 14:18:03 +0100

    ceny pop

--- app/zamowic/page.js
+++ app/zamowic/page.js
@@ -8,15 +8,15 @@ function order() {
                     Masz kilka sposobów, aby zamówić składanie komputera u mnie:<br></br><br></br>
 1: Masz już gotową konfigurację
 Jeśli już wiesz, jakie komponenty chcesz, podpisujemy z Tobą umowę online, a następnie przesyłasz mi swoją konfigurację. Potem możesz wysłać lub zamówić pod mój adres komponenty, ja sprawdzam ich działanie, składam komputer, instaluję system Windows (bez aktywacji), aktywuję profil XMP i wykonuję undervolting procesora (zmniejszenie napięcia przy zachowaniu pełnej wydajności, co skutkuje niższą temperaturą CPU). Następnie instaluję sterowniki i wykonuję testy stresowe, aby upewnić się, że wszystko działa stabilnie.
- Cena usługi: 250 zł dla komputerów do 4500 zł, powyżej 4500 zł - 7% wartości komputera.
+ Cena usługi: 250 zł dla komputerów do 6000 zł, powyżej 6000 zł - 5% wartości komponentów.
 <br/><br></br>
 2: Wybierasz procesor + kartę graficzną
 Podajesz mi parę procesor + GPU, podpisujemy umowę, a ja dobieram pozostałe komponenty tak, aby najlepiej pasowały do Twojego budżetu i potrzeb. Następnie składam komputer i wykonuję wszystkie czynności z opcji 1
 
- Cena usługi: 300 zł dla komputerów do 4500 zł, powyżej 4500 zł - 8% wartości komponentów.<br/><br></br>
+ Cena usługi: 300 zł dla komputerów do 6000 zł, powyżej 6000 zł - 6% wartości komponentów.<br/><br></br>
 3: Nie wiesz, jaki procesor + karta graficzna będą najlepsze
 Mówisz mi swoje potrzeby - ja proponuję odpowiednią parę CPU + GPU, podpisujemy umowę, a ja dobieram całą konfigurację i składam komputer tak jak w opcji 1.
- Cena usługi: 300 zł dla komputerów do 4500 zł, powyżej 4500 zł -8% wartości komponentów.<br></br><br></br>
+ Cena usługi: 300 zł dla komputerów do 6000 zł, powyżej 6000 zł - 6% wartości komponentów.<br></br><br></br>
 Aby złożyć zamówienie, zapraszam do kontaktu!
 
 
