b5d64ed3b1f126a9b11d3642e7d473cade8445a7
Михаил Лядский <lmp200840@gmail.com>
2026-05-27 10:35:26 +0200

    add youtube

--- app/components/contact.js
+++ app/components/contact.js
@@ -7,6 +7,7 @@ function Contact(props) {
         <p className={`${styles.contact_inf} ${styles.fb}`}>Facebook: <a href="https://www.facebook.com/share/1bJiAygM8X/" className={styles.cl}>kliknij</a></p>
         <p className={`${styles.contact_inf} ${styles.inst}`}>Instagram: <a href="https://www.instagram.com/build_my_pc256?igsh=MWpzZG81N29yeTcxZA=="className={styles.cl}>kliknij</a></p>
         <p className={`${styles.contact_inf} ${styles.tg}`}>TikTok: <a href="https://www.tiktok.com/@build_my_pc?is_from_webapp=1&sender_device=pc"className={styles.cl}>kliknij</a></p>
+        <p className={`${styles.contact_inf} ${styles.tg}`}>Youtube: <a href="https://www.youtube.com/@Build_My_PC"className={styles.cl}>kliknij</a></p>
     </div>
     </>);
 }
